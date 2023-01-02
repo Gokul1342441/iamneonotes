1eaa3375e5dada8df5b0a30117819f4a2220e1cc
gokul <72244656+Gokul1342441@users.noreply.github.com>
2023-01-02 19:06:45 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+     <div className="logo"><img src="https://iamneo.ai/wp-content/uploads/2022/07/iamneo-logo.png" alt="" className="logo1"/></div>
+    </header>
+  );
+}
+
+export default Header;
